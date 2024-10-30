7aa7d5afc6e2ed3d77ccedd7d768bd624517aacd
LogiFlutter <hoangnhatlong17042000@gmail.com>
2024-10-30 22:52:39 +0700

    update

--- lib/controller/post_controller.dart
+++ lib/controller/post_controller.dart
@@ -107,6 +107,121 @@ class PostController extends GetxController implements GetxService {
     return response.statusCode!;
   }
 
+  Future<void> getImage(String nameFile) async {
+    Response response = await repo.getImage(nameFile);
+
+    if (response.statusCode == 200) {
+      final Directory tempDir = await getTemporaryDirectory();
+      File file = File('${tempDir.path}/$nameFile');
+
+      if (response.bodyString != null) {
+        file = await file.writeAsBytes(response.bodyString!.codeUnits);
+        _mapFileAvatar[nameFile] = file;
+      }
+    } else {
+      ApiChecker.checkApi(response);
+    }
+  }
+
+  Future<int?> likePost(
+    final DateTime? date,
+    final PostEntity? objPost,
+  ) async {
+    LikeEntity likeEntity = LikeEntity(
+        id: null, type: 0, date: DateTime.now(), objPost: objPost, user: _user);
+
+    bool? isLiked = objPost!.likes.any((e) => e.user?.id == _user?.id);
+
+    if (!isLiked) {
+      objPost.likes.add(LikeEntity(date: date, user: _user));
+      update();
+
+      Response response = await repo.likePost(likeEntity);
+
+      if (response.statusCode != 200) {
+        ApiChecker.checkApi(response);
+        // back laji
+        if (!isLiked) {
+          objPost.likes.removeWhere((e) => e.user?.id == _user?.id);
+        } else {
+          objPost.likes
+              .add(LikeEntity(date: date, id: null, type: null, user: _user));
+        }
+      }
+      return response.statusCode!;
+    } else {
+      objPost.likes.removeWhere((e) => e.user?.id == _user?.id);
+      update();
+      //Thiếu endpoint unlike
+    }
+    return 400;
+  }
+
+  Future<int> sendComment(String content, PostEntity objPost) async {
+    CommentEntity objComment = CommentEntity(
+      content: content,
+      date: DateTime.now(),
+      objPost: objPost,
+      user: _user,
+    );
+
+    objPost.comments.add(objComment);
+    update();
+
+    Response response = await repo.comment(objComment);
+    if (response.statusCode != 200) {
+      objPost.comments.remove(objComment);
+      ApiChecker.checkApi(response);
+    }
+
+    update();
+    return response.statusCode!;
+  }
+
+  Future<int> editPost(
+    PostEntity objPost,
+    String newContent,
+  ) async {
+    objPost = objPost.copyWith(content: newContent, media: [
+      //Em để test ạ
+      MediaEntity(
+        id: 0,
+        contentType: 'application/octet-stream',
+        contentSize: 237056,
+        name: '2024-10-29 06:24:27.664070.png',
+        extension: null,
+        filePath:
+            'src/main/resources/uploads/images/2024-10-29 06:24:27.664070.png',
+        isVideo: null,
+        objPost: null,
+      ),
+    ]);
+
+    Response response = await repo.editPost(objPost);
+
+    if (response.statusCode == 200) {
+      objPost = PostEntity.fromJson(response.body['data']);
+
+      final index = _posts!.indexWhere((e) => e.id == objPost.id);
+      posts![index] = objPost;
+    } else {
+      ApiChecker.checkApi(response);
+    }
+
+    update();
+    return response.statusCode!;
+  }
+
+  void addXFile(List<XFile> xFiles) {
+    _mediaFiles = xFiles;
+    update();
+  }
+
+  void removeXfile() {
+    _mediaFiles = null;
+    update();
+  }
+
   Future<int> createPost({
     required String content,
   }) async {
@@ -166,118 +281,5 @@ class PostController extends GetxController implements GetxService {
     }
 
     return null;
-  }
-
-  Future<void> getImage(String nameFile) async {
-    Response response = await repo.getImage(nameFile);
-
-    if (response.statusCode == 200) {
-      final Directory tempDir = await getTemporaryDirectory();
-      File file = File('${tempDir.path}/$nameFile');
-
-      if (response.bodyString != null) {
-        file = await file.writeAsBytes(response.bodyString!.codeUnits);
-        _mapFileAvatar[nameFile] = file;
-      }
-    } else {
-      ApiChecker.checkApi(response);
-    }
-  }
-
-  Future<int> likePost(
-    final DateTime? date,
-    final PostEntity? objPost,
-  ) async {
-    LikeEntity likeEntity = LikeEntity(
-        id: null, type: 0, date: DateTime.now(), objPost: objPost, user: _user);
-
-    bool? isLiked = objPost!.likes.any((e) => e.user?.id == _user?.id);
-    if (!isLiked) {
-      objPost.likes.add(LikeEntity(date: date, user: _user));
-    } else {
-      objPost.likes.removeWhere((e) => e.user?.id == _user?.id);
-    }
-    update();
-
-    Response response = await repo.likePost(likeEntity);
-
-    if (response.statusCode != 200) {
-      ApiChecker.checkApi(response);
-      //back laji
-      // if (!isLiked) {
-      //   objPost.likes.removeWhere((e) => e.user?.id == _user?.id);
-      // } else {
-      //   objPost.likes
-      //       .add(LikeEntity(date: date, id: null, type: null, user: _user));
-      // }
-    }
-    return response.statusCode!;
-  }
-
-  Future<int> sendComment(String content, PostEntity objPost) async {
-    CommentEntity objComment = CommentEntity(
-      content: content,
-      date: DateTime.now(),
-      objPost: objPost,
-      user: _user,
-    );
-
-    objPost.comments.add(objComment);
-    update();
-
-    print('zzz88');
-    Response response = await repo.comment(objComment);
-    print('zzz89');
-    if (response.statusCode != 200) {
-      objPost.comments.remove(objComment);
-      ApiChecker.checkApi(response);
-    }
-
-    update();
-    return response.statusCode!;
-  }
-
-  Future<int> editPost(
-    PostEntity objPost,
-    String newContent,
-  ) async {
-    objPost = objPost.copyWith(content: newContent, media: [
-      //Em để test ạ
-      MediaEntity(
-        id: 0,
-        contentType: 'application/octet-stream',
-        contentSize: 237056,
-        name: '2024-10-29 06:24:27.664070.png',
-        extension: null,
-        filePath:
-            'src/main/resources/uploads/images/2024-10-29 06:24:27.664070.png',
-        isVideo: null,
-        objPost: null,
-      ),
-    ]);
-
-    Response response = await repo.editPost(objPost);
-
-    if (response.statusCode == 200) {
-      objPost = PostEntity.fromJson(response.body['data']);
-
-      final index = _posts!.indexWhere((e) => e.id == objPost.id);
-      posts![index] = objPost;
-    } else {
-      ApiChecker.checkApi(response);
-    }
-
-    update();
-    return response.statusCode!;
-  }
-
-  void addXFile(List<XFile> xFiles) {
-    _mediaFiles = xFiles;
-    update();
-  }
-
-  void removeXfile() {
-    _mediaFiles = null;
-    update();
   }
 }
